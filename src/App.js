65a9e40f9db3e4f3fac25b6037d6d32da24da2ff
S3759362rmit <s3759362@student.rmit.edu.au>
2021-08-21 11:47:18 +1000

    add static variable state to switch chat stages.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chatbot from 'react-chatbot-kit'
 import './App.css';
 
@@ -7,6 +7,8 @@ import MessageParser from './MessageParser';
 import config from './config';
 
 function App() {
+  // const [chatState, setChatState] = useState("initial");
+
   return (
     <div className="App">
       <header className="App-header">
